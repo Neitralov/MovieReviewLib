aa5634d16c82bf8b6998072990db550b2169ac11
Neitralov <maxim.neitralov@gmail.com>
2023-05-07 19:34:17 +0700

    Теперь перед публикацией проверяются длительность фильма и год выпуска

--- Data/Movie.cs
+++ Data/Movie.cs
@@ -5,27 +5,43 @@ namespace MovieReviewLib.Data;
 public class Movie
 {
     public int Id { get; set; }
+    
     public bool WatchLater { get; set; }
+    
     [Required(ErrorMessage = "Отсутствует обложка!")]
     public string PosterPath { get; set; } = string.Empty;
+    
     public MovieType Type { get; set; }
+    
     [Required(ErrorMessage = "Название фильма не может быть пустым!")]
     public string Name { get; set; } = string.Empty;
+    
     [Required(ErrorMessage = "Год выпуска не может быть пустым!")]
+    [Range(1000, 9999, ErrorMessage = "Значение года должно иметь длину 4 символа!")]
     public int? ReleaseYear { get; set; }
-    [Required(ErrorMessage = "Длительность фильма не может быть пустой!")]
+    
+    [Required(ErrorMessage = "Длительность не может быть пустой!")]
+    [Range(1, int.MaxValue, ErrorMessage = "Длительность не может быть меньше одной минуты!")]
     public int? Duration { get; set; }
+    
     [MinLength(1, ErrorMessage = "У фильма не может отсутствовать жанр!")]
     public List<Genre> Genres { get; set; } = new();
+    
     [MinLength(1, ErrorMessage = "У фильма не может отсутствовать режиссер!")]
     public List<Director> Directors { get; set; } = new();
+    
     [MinLength(1, ErrorMessage = "У фильма не может отсутствовать сценарист!")]
     public List<Writer> Writers { get; set; } = new();
+    
     [MinLength(1, ErrorMessage = "У фильма не может отсутствовать композитор!")]
     public List<Compositor> Compositors { get; set; } = new();
+    
     [MinLength(1, ErrorMessage = "У фильма не может отсутствовать актер!")]
     public List<Actor> Actors { get; set; } = new();
+    
     public int Score { get; set; }
+    
     public string Note { get; set; } = string.Empty;
+    
     public DateTime? PublishDate { get; set; }
 }
