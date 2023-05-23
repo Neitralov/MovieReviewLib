db28308ef57aa41e58ec86634f5ef293a0664a16
Neitralov <maxim.neitralov@gmail.com>
2023-05-23 20:54:02 +0700

    Добавлена категория Шоу

--- Data/MovieType.cs
+++ Data/MovieType.cs
@@ -4,5 +4,6 @@ public enum MovieType
 {
     Film,
     Series,
+    Show,
     Cartoon
 }
---
+++ Migrations/20230523130821_AddShowTypeInMovieType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieReviewLib.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShowTypeInMovieType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
