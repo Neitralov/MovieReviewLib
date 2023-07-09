a7649ca56ff8c71ea5dfbc99400123c83af32602
Neitralov <maxim.neitralov@gmail.com>
2023-07-09 16:04:36 +0700

    Миграции удалены

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -5,13 +5,11 @@ namespace MovieReviewLib.Data;
 public sealed class DatabaseContext : DbContext
 {
     public DbSet<Movie> Movies => Set<Movie>();
-
-    //При миграции закомментировать конструктор
+    
     public DatabaseContext() => Database.EnsureCreated();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //При миграциях установить: optionsBuilder.UseSqlite("Data Source=tmp/Database/Database.db");
         optionsBuilder.UseSqlite("Data Source=Database/Database.db");
     }
 }
